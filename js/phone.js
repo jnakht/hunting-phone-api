0b54edf2c0eeff9c7817264b7d84f558db22509b
jnakht <jisan.cse.cu@gmail.com>
2025-02-02 10:06:41 +0600

    show all feature added

--- js/phone.js
+++ js/phone.js
@@ -1,13 +1,24 @@
 
-const loadPhone = async(searchText) => {
+const loadPhone = async(searchText, isShowAll) => {
     const res = await fetch(`https://openapi.programming-hero.com/api/phones?search=${searchText}`);
     const data = await res.json();
     // console.log(data.data)
-    displayPhones(data.data);
+    displayPhones(data.data, isShowAll);
 }
 
-const displayPhones = phones => {
+const displayPhones = (phones, isShowAll) => {
     const phoneContainer = document.getElementById('phone-container');
+    phoneContainer.textContent = '';
+    const showAllContainer = document.getElementById('show-all-container');
+    if (phones.length > 12 && !isShowAll) {
+        showAllContainer.classList.remove('hidden');
+    }
+    else {
+        showAllContainer.classList.add('hidden');
+    }
+    if (!isShowAll) {
+        phones = phones.slice(0, 12);
+    }
     phones.forEach(phone => {
         console.log(phone)
         const phoneCard = document.createElement('div');
@@ -25,13 +36,31 @@ const displayPhones = phones => {
             </div>
         </div>`;
         phoneContainer.appendChild(phoneCard);
-    })
+    });
+    // toggleLoaderSpinner(false);
     
 }
 
-const handleSearch = () => {
+// spinner stops too soon because API call function does not wait to load all data, since it is asynchronous function. using async and await fix this problem.
+const handleSearch = async (isShowAll) => {
+    toggleLoaderSpinner(true);
     const searchField = document.getElementById('search-field');
     const searText = searchField.value;
-    loadPhone(searText);
+    await loadPhone(searText, isShowAll);
+    toggleLoaderSpinner(false);
+
 }
 
+const toggleLoaderSpinner = (isLoading) => {
+    const loader = document.getElementById('loader-spinner');
+    if (isLoading) {
+        loader.classList.remove('hidden');
+    }
+    else {
+        loader.classList.add('hidden');
+    }
+}
+
+const handleShowAll = () => {
+    handleSearch(true);
+}
